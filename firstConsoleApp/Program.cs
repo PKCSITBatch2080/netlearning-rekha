810ac81c71a4bd2d19dea9833a7adcef2be58d80
rekhaaryal <aryalr458@gmail.com>
2024-03-05 19:53:28 +0545

    learn fileio

--- firstConsoleApp/Program.cs
+++ firstConsoleApp/Program.cs
@@ -1,34 +1,36 @@
 ﻿using System;
 
-Console.WriteLine(36886765);
+// Console.WriteLine(36886765);
 
-DataTypes dt = new();
-dt.Learn();
+// DataTypes dt = new();
+// dt.Learn();
 
-Methods m = new();
-m.Greet("Hi");
-m.CalculateBMI(222.2f, 333.33f);
+// Methods m = new();
+// m.Greet("Hi");
+// m.CalculateBMI(222.2f, 333.33f);
 
-var highest = m.GetMax(3.4, 5.6, 78.4, 23.4);
-var highest2 = m.GetMax(3.4, 5.6);
-var highest3 = m.GetMax(3.4, 5.6, 78.4, 23.4, 67.8);
-var highest4 = m.GetMax(3.4);
+// var highest = m.GetMax(3.4, 5.6, 78.4, 23.4);
+// var highest2 = m.GetMax(3.4, 5.6);
+// var highest3 = m.GetMax(3.4, 5.6, 78.4, 23.4, 67.8);
+// var highest4 = m.GetMax(3.4);
 
-Generics g = new();
-g.Display<string>(" Rekha ");
-g.Display<int>(888);
+// Generics g = new();
+// g.Display<string>(" Rekha ");
+// g.Display<int>(888);
 
-Student rama = new Student();
-Student rekha = new Student("Rekha Aryal", 123);
+// Student rama = new Student();
+// Student rekha = new Student("Rekha Aryal", 123);
 
-rama.RollNumber = 22;      // Assined
-rekha.RollNumber = 223;    // Not Assigned
-//rama.Dob = DateTime.Now;
-CSStudent roshan = new("Roshan Aryal", 19 ,"Hospital management system");
-roshan.DisplayInfo();
+// rama.RollNumber = 22;      // Assined
+// rekha.RollNumber = 223;    // Not Assigned
+// //rama.Dob = DateTime.Now;
+// CSStudent roshan = new("Roshan Aryal", 19 ,"Hospital management system");
+// roshan.DisplayInfo();
 
-var Sapana = new CyberSecurityStudent("Sapana Shrestha", 11 , "HMS" , "RedHat");
-Sapana.DisplayInfo();
+// var Sapana = new CyberSecurityStudent("Sapana Shrestha", 11 , "HMS" , "RedHat");
+// Sapana.DisplayInfo();
 
+//CyberSecurityStudent.Do();
+FDHandling.CreateFile();//static member haru class ko hunxa object ko hunna
 
 
